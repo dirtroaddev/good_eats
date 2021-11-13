3c28694a80907a36fc42bf69b0747b9d7cc65bbc
fullstackjunkie <rongoodwindev@gmail.com>
2021-11-13 14:01:30 -0500

    Fixing Files

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Header = () => {
+    return(
+        <div>
+            <div className="font-weight-light display-1 text-center">Restaurant Finder</div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const RestaurantDetailPage = () => {
+    return (
+        <div>Restaurant</div>
+    )
+}
+
+
+export default RestaurantDetailPage;
